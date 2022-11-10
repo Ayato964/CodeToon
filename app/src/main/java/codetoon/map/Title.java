964004c25829acf09bfc207daf5d3481ead9a54a
Ayato <chtk20063@g.nihon-u.ac.jp>
2022-11-11 02:24:27 +0900

    Animation.java Fade.java -正しくフェードイン とフェードアウトしない問題を修正

--- app/src/main/java/codetoon/map/Title.java
+++ app/src/main/java/codetoon/map/Title.java
@@ -15,7 +15,7 @@ public class Title extends Map{
         switch(i){
           case 0:
           Server.isHost = true; Main.getInstance().run(new CreateSection());Server.server.startServer(null);
-             /* Main.getInstance().run(new PazzleStage(5)); Server.server.setUpServer();*/ break;
+             /* Main.getInstance().run(new PazzleStage(5)); /*Server.server.setUpServer();*/ break;
           case 1: System.out.println("途中からゲームを始める。");Server.isHost = false; Server.server.startServer("192.168.11.13"); break;
           case 2: System.exit(0); break;
         }
