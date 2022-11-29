9fdd96c8c26d9d132c3970eb4db38ac627b5d4d4
Ayato <chtk20063@g.nihon-u.ac.jp>
2022-11-29 13:17:11 +0900

    Attack.java -引数に、配列の座標を入力することで、特定のメモリーを撃破するようにした。

--- app/src/main/java/codetoon/method/Attack.java
+++ app/src/main/java/codetoon/method/Attack.java
@@ -2,14 +2,16 @@ package codetoon.method;
 
 import java.util.HashMap;
 
+import codetoon.argument.IntegerArgument;
 import codetoon.server.Server;
 import codetoon.system.*;
 
 public class Attack extends MyMethod{
+    int x, y;
     @Override
     public void action(int i) {
         System.out.println("attack");
-        Memorys.opponentMemory.get(0).changeColor();
+        Memorys.opponentMemory.get(y * CodeToon.MEMORY_SIZE + x).changeColor();
         Server.server.sendOpponentCopy();
     }
     @Override
@@ -17,7 +19,11 @@ public class Attack extends MyMethod{
         return 1;
     }
     @Override
-    public String set(HashMap<Integer, String> map) {
+    public String set(HashMap<Integer, String> map)
+    {
+        x = IntegerArgument.getInstance().indentification(map.get(0));
+        y = IntegerArgument.getInstance().indentification(map.get(1));
+
         return null;
     }
     @Override
