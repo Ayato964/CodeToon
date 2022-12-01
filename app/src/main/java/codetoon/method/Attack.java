7db1a02bdd9813b2a9dc433368d4e995d318b3a5
Ayato <chtk20063@g.nihon-u.ac.jp>
2022-12-01 16:40:27 +0900

    Bug_fix

--- app/src/main/java/codetoon/method/Attack.java
+++ app/src/main/java/codetoon/method/Attack.java
@@ -19,7 +19,6 @@ public class Attack extends MyMethod{
         if(enemy instanceof Memory){
             ((Memory) enemy).hacking(pass);
         }
-        Server.server.sendOpponentCopy();
     }
     @Override
     public int getCount() {
