9a64413c788c1e2fa36531ebe4d9afbcd294a929
Ayato <chtk20063@g.nihon-u.ac.jp>
2022-12-01 16:58:50 +0900

    Bug_fix

--- app/src/main/java/codetoon/server/Server.java
+++ app/src/main/java/codetoon/server/Server.java
@@ -126,7 +126,7 @@ public class Server implements Runnable {
         try {
             testClassWrapper testWrapper = new testClassWrapper(Memorys.memory);
             myOutStream.reset();
-            System.out.println("SendCopy:" + testWrapper.memory.get(0).getName() + "    " + Memorys.memory.get(0).showPass());
+            System.out.println("SendCopy:" + testWrapper.memory.get(0).getName() + "    " + testWrapper.memory.get(0).showPass());
             myOutStream.writeObject(testWrapper);
 
         } catch (IOException e) {
