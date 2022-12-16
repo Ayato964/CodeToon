2e5f3bca9298d799c555a2512f37b8a849a22406
Ayato <chtk20063@g.nihon-u.ac.jp>
2022-12-16 18:41:20 +0900

    bug_fix

--- app/src/main/java/codetoon/main/Main.java
+++ app/src/main/java/codetoon/main/Main.java
@@ -52,7 +52,7 @@ public class Main extends JFrame{
     }
     /** Main Method !!! **/
     public static void main(String[] args) {
-        LangLoader.create("zh_cn");
+        LangLoader.create("ja_jp");
         System.out.println();
         Main m = new Main("CodeToon");
         m.setVisible(true);
