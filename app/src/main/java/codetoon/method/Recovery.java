fa251c2f28c059c61bd4ae794ff355e661e6cee7
Ayato <chtk20063@g.nihon-u.ac.jp>
2023-01-01 11:47:29 +0900

    Ver.1.1.4
    for文とrecoveryを組み合わせたときにおこるバグを修正

--- app/src/main/java/codetoon/method/Recovery.java
+++ app/src/main/java/codetoon/method/Recovery.java
@@ -10,8 +10,8 @@ import org.jetbrains.annotations.NotNull;
 import java.util.HashMap;
 
 public class Recovery extends MyMethod{
-    private Memory memory;
-    private int pass;
+    private String Stringmemory;
+    private String stringPass;
     @Override
     public Object newInstance() {
         return new Recovery();
@@ -19,14 +19,16 @@ public class Recovery extends MyMethod{
 
     @Override
     public String set(@NotNull HashMap<Integer, String> map) {
-        memory = (Memory) ObjectArgument.getInstance().indentification(map.get(0));
-
-        pass =map.get(1) != null ?  IntegerArgument.getInstance().indentification(map.get(1)) : 0;
+        Stringmemory = map.get(0);
+        stringPass = map.get(1);
         return null;
     }
 
     @Override
     public void action(int i) {
+        Memory memory = (Memory) ObjectArgument.getInstance().indentification(Stringmemory);
+        int pass =  stringPass != null ?  IntegerArgument.getInstance().indentification(stringPass) : 0;
+
         memory.recovering(pass);
     }
 }
