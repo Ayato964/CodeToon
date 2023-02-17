202cb957e301a44f0ae4e9585d586059bf0bc976
Ayato <chtk20063@g.nihon-u.ac.jp>
2023-02-18 01:53:12 +0900

    Ver.1.7.16
    If文のバグを修正

--- app/src/main/java/codetoon/method/If.java
+++ app/src/main/java/codetoon/method/If.java
@@ -13,6 +13,8 @@ public class If extends MyMethod{
     boolean isBool;
     ArrayList<MyMethod> methods;
     String inside;
+    String stringBool;
+
     @Override
     public Object newInstance() {
         return new If();
@@ -20,7 +22,7 @@ public class If extends MyMethod{
 
     @Override
     public String set(@NotNull HashMap<Integer, String> map) {
-        isBool = BooleanArgument.getInstance().indentification(map.get(0));
+        stringBool = map.get(0);
         inside = map.get(CodeToon.INSIDE_METHODS);
         return null;
     }
@@ -29,6 +31,7 @@ public class If extends MyMethod{
     public void action(Player host) {
         methods = ConvertSource.convert(inside, host);
         methods = host.removeBlackList(methods);
+        isBool = BooleanArgument.getInstance().indentification(stringBool, host);
         if(isBool){
             for(int c = 0; c < methods.size(); c ++){
                 methods.get(c).action(host);
